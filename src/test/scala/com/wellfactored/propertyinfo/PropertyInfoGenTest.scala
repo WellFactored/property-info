2cd1839d4fb8b2fd65692f3000327ab80bd7547e
Doug Clinton <doug@wiredthing.com>
2016-09-14 11:17:55 +0100

    Added another test.

--- src/test/scala/com/wellfactored/propertyinfo/PropertyInfoGenTest.scala
+++ src/test/scala/com/wellfactored/propertyinfo/PropertyInfoGenTest.scala
@@ -9,6 +9,11 @@ class PropertyInfoGenTest extends WordSpecLike with Matchers with PropertyInfoGe
       case class Foo(a: String, b: Int)
       implicitly[PropertyInfo[Foo]].namesAndTypes.map(p => (p.name, p.ty.describe)) shouldBe List(("a", "String"), ("b", "Int"))
     }
+
+    "handle options" in {
+      case class Foo(a: String, b: Option[String])
+      implicitly[PropertyInfo[Foo]].namesAndTypes.map(p => (p.name, p.ty.describe)) shouldBe List(("a", "String"), ("b", "Option[String]"))
+    }
   }
 
 }
